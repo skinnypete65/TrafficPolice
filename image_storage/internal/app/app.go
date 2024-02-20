b16668a7900bfc309f05651c9c46f0a563eb9d90
skinnypete65 <skinnypetetskipe@gmail.com>
2024-02-20 18:05:27 +0300

    add dockerfile for image storage

--- image_storage/internal/app/app.go
+++ image_storage/internal/app/app.go
@@ -15,9 +15,12 @@ func Run() {
 
 	mux.HandleFunc("POST /case/{id}/img", handler.UploadCaseImg)
 	mux.HandleFunc("GET /case/{id}/img", handler.GetCaseImg)
+	mux.HandleFunc("GET /test", func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte("OK!"))
+	})
 
 	server := http.Server{
-		Addr:    "localhost:8000",
+		Addr:    ":8000",
 		Handler: mux,
 	}
 
